2991bc60605668a782be5398dc90ae05cf9a43ed
Daniele De Lorenzi <d.delorenzi@endian.com>
2019-09-17 21:16:35 +0200

    print packet data based on their packetID

--- f1_telemetry/server.py
+++ f1_telemetry/server.py
@@ -4,9 +4,11 @@ from f1_telemetry.f1_2019_struct import *
 UDP_IP = "0.0.0.0"
 UDP_PORT = 20777
 
+
 def get_telemetry():
     """
-    Generator function which yields UDPPackets from the specified ip address and port
+    Generator function which yields UDPPackets from
+    the specified ip address and port
 
     :yield: A a packet send by F1 2019
     """
@@ -17,28 +19,39 @@ def get_telemetry():
     while True:
         data, _ = sock.recvfrom(1347)
         m_header = Header.from_buffer_copy(data[0:23])
+        mad = 50
         if int(m_header.m_packetId) == 0:
             packet = PacketMotionData.from_buffer_copy(data[0:1343])
+            theader = int(m_header.m_packetId)
 
         elif int(m_header.m_packetId) == 1:
             packet = PacketSessionData.from_buffer_copy(data[0:149])
+            theader = int(m_header.m_packetId)
 
         elif int(m_header.m_packetId) == 2:
             packet = PacketLapData.from_buffer_copy(data[0:843])
+            theader = int(m_header.m_packetId)
+            #mad = PacketLapData.from_buffer_copy(data[0:843])
 
         elif int(m_header.m_packetId) == 3:
             packet = PacketEventData.from_buffer_copy(data[0:32])
+            theader = int(m_header.m_packetId)
 
         elif int(m_header.m_packetId) == 4:
             packet = PacketParticipantsData.from_buffer_copy(data[0:1104])
+            theader = int(m_header.m_packetId)
+            mad = PacketParticipantsData.from_buffer_copy(data[0:1104])
 
         elif int(m_header.m_packetId) == 5:
             packet = PacketCarSetupData.from_buffer_copy(data[0:843])
+            theader = int(m_header.m_packetId)
 
         elif int(m_header.m_packetId) == 6:
             packet = PacketCarTelemetryData.from_buffer_copy(data[0:1347])
+            theader = int(m_header.m_packetId)
 
         elif int(m_header.m_packetId) == 7:
             packet = PacketCarStatusData.from_buffer_copy(data[0:1143])
+            theader = int(m_header.m_packetId)
 
-        yield packet
+        yield packet, theader, mad
--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 from f1_telemetry.server import get_telemetry
+#from f1_telemetry.f1_2019_struct import *
 import time
 
 if __name__ == '__main__':
     print("Server started on 20777")
-    for packet in get_telemetry():
-        #print(packet)
-        try:
-            print("Track: ", packet.m_trackId, "Car Type: ", packet.m_formula,
+    for packet, theader, madonna in get_telemetry():
+        #print(theader, packet)
+        if theader == 1:
+            print(theader, "Track: ", packet.m_trackId,
+                  "Car Type: ", packet.m_formula,
                   "Safety Car: ", packet.m_safetyCarStatus,
                   "Weather:", packet.m_weather,
                   "Track Temp: ", packet.m_trackTemperature,
                   "Air Temp: ", packet.m_airTemperature,
                   "Total Laps: ", packet.m_totalLaps, time.time())
-            print("Test Lap:", packet.m_lastLapTime, packet.m_currentLapTime,
-                  packet.m_bestLapTime, packet.m_sector1Time,
-                  packet.m_sector2Time, packet.m_lapDistance,
-                  packet.m_totalDistance)
-            print("Test Teams:", packet.m_aiControlled, packet.m_driverId,
-                  packet.m_teamId, packet.m_raceNumber,
-                  packet.m_nationality, packet.m_name,
-                  packet.m_yourTelemetry)
-        except:
-            continue
+
+        elif theader == 2:
+            print("Header ID: ", theader)
+            #print(dir(madonna))
+            #print(dir(packet.m_lapsData))
+            #print(packet.m_lapsData.__dict__)
+            #print(theader, "Test Lap:", #packet.m_lastLapTime,
+                  #packet.m_currentLapTime,
+                  #packet.m_bestLapTime, packet.m_sector1Time,
+                  #packet.m_sector2Time, packet.m_lapDistance,
+                  #packet.m_totalDistance,
+            #      packet.m_lapsData)
+        elif theader == 3:
+            print(dir(packet.m_eventStringCode))
+            print(theader, "Event ID: ", packet.m_eventStringCode._type_)
+        elif theader == 4:
+            print(theader, "Active Cars: ", packet.m_numActiveCars)
+            #print(theader, "Test Teams:", packet.m_aiControlled,
+            #      packet.m_driverId,
+            #      packet.m_teamId, packet.m_raceNumber,
+            #      packet.m_nationality, packet.m_name,
+            #      packet.m_yourTelemetry)
+        #elif theader == 5:
+        #    print(packet)
